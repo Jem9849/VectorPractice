0907146e3d5956dc9c303fe76822e00cbccf4ec5
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-01-29 13:08:29 -0700

    Add vectors of int and string and pushed values and then printed those values through loop.

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -7,6 +7,7 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #include "Controller.hpp"
@@ -18,6 +19,21 @@ Controller :: Controller()
 
 void Controller :: start()
 {
+	vector <int> loop1;
+	vector <string> loop2;
 
+	for (int i = 0; i<=14; i++)
+	{
+		loop1.push_back(i);
+
+		cout << "Element pushed: " << loop1[i] << endl;
+	}
+
+	for (int i = 0; i<16; i++)
+	{
+		loop2.push_back("Cats");
+
+		cout << "There are " << loop2[i] << "." << endl;
+	}
 }
 
